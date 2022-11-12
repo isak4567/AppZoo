b5d079fe6b7d7a192c208c8ede0558b6a9e2de5f
Isaias RoblesVega <isak4567@gmail.com>
2022-11-11 23:57:58 -0300

    Fase inicial de pagina

--- Appzoologico/models.py
+++ Appzoologico/models.py
@@ -5,9 +5,14 @@ class Animal_GrupoPrincipal (models.Model):
 
     nombre = models.CharField(max_length = 40)
     extremidades = models.IntegerField()
-    tipo = models.CharField(max_length = 15)
-    alimentacion = models.CharField(max_length = 15)
-    conducta = models.CharField(max_length = 150)
+    AlimentacionElecciones = [
+        ('Omnívoro', 'Omnívoro'),
+        ('Carnívoro', 'Carnívoro'),
+        ('Herbívoro', 'Herbívoro'),
+        ('Insectivoros', 'Insectivoros'),
+    ]
+    alimentacion = models.CharField(max_length = 12, choices = AlimentacionElecciones, default='Omnívoro')
+    conducta = models.CharField(max_length = 45)
 
     class Meta:
         abstract = True
@@ -15,10 +20,34 @@ class Animal_GrupoPrincipal (models.Model):
 class Animal_GrupoSecundario1 (Animal_GrupoPrincipal):
 
     Subfilo_Vertebrado = "Vertebrado"
+    EspecieElecciones = [
+        ('Mamifero', 'Mamifero'),
+        ('Aves', 'Aves'),
+        ('Reptil', 'Reptil'),
+        ('Anfibio', 'Anfibio'),
+        ('Pez', 'Pez'),
+    ]
+    Especie = models.CharField(
+        max_length= 8,
+        choices= EspecieElecciones,
+        default= 'Mamifero',
+    )
 
 class Animal_GrupoSecundario2 (Animal_GrupoPrincipal):
 
     Subfilo_Invertebrado = "Invertebrado"
+    EspecieElecciones = [
+        ('Insecto', 'Insecto'),
+        ('Araña', 'Araña'),
+        ('Molusco', 'Molusco'),
+        ('Crustaceo', 'Crustaceo'),
+        ('Myriapoda', 'Myriapoda'),
+    ]
+    Especie = models.CharField(
+        max_length= 9,
+        choices= EspecieElecciones,
+        default= 'Insectos',
+    )
 
 class Empleado (models.Model):
 

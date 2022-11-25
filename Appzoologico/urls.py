26453bb5b2729e2d422ddc436173b94258f00dad
Isaias RoblesVega <isak4567@gmail.com>
2022-11-25 20:03:29 -0300

    Actualizando

--- Appzoologico/urls.py
+++ Appzoologico/urls.py
@@ -13,22 +13,28 @@ from django.contrib.auth.views import LogoutView
 
 
 urlpatterns = [
+    # ---------------------------- Urls Principales ----------------------------
     path('', view_zool, name="Home"),
     path('Posts/', view_posts, name="posts"),
     path('About/', view_about, name="about"),
     path('Contact/', view_contact, name="contact"),
     path('Registrarse/', view_Registrarse, name="RegistrarCuenta"),
+    # ---------------------------- Urls Log ----------------------------
     path('LogIn/', view_Login, name="LogIn"),
     path('ActualizarPerfil/', view_Editar_Perfil, name="ActualizarPerfil"),
     path('logout/', LogoutView.as_view(template_name='IndexZoo.html'), name='LogOut'),
+    # ---------------------------- Urls AnimalG1 ----------------------------
     path('AgregarAnimalG1/', view_AgregarAnimalG1, name="AgregarAnimalG1"),
     path('DetallesAnimalG1/<int:id>', view_DetallesAnimalG1, name="DetallesAnimalG1"),
     path('ModificarAnimalG1/<int:id>', view_ModificarAnimalG1, name="ModificarAnimalG1"),
     path('EliminarAnimalG1/<int:id>', view_EliminarAnimalG1, name="EliminarAnimalG1"),
+    # ---------------------------- Urls AnimalG2 ----------------------------
     path('AgregarAnimalG2/', view_AgregarAnimalG2, name="AgregarAnimalG2"),
     path('DetallesAnimalG2/<int:id>', view_DetallesAnimalG2, name="DetallesAnimalG2"),
     path('ModificarAnimalG2/<int:id>', view_ModificarAnimalG2, name="ModificarAnimalG2"),
     path('EliminarAnimalG2/<int:id>', view_EliminarAnimalG2, name="EliminarAnimalG2"),
+    # ---------------------------- Urls Lista Animales ----------------------------
     path('ListasAnimales/', view_ListasAnimales, name="ListasAnimales"),
+    # ---------------------------- Urls Post ----------------------------
     path('AgregarPost/<nombre>', agregarpost, name="AgregarPost"),
 ]
